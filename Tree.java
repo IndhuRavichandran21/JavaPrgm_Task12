8ea9046f5047be6c268c9a5469b620b7e7e669ae
IndhuRavichandran21 <indhuravi21@gmail.com>
2024-04-19 22:43:41 +0530

    Commitng file

--- Tree.java
+++ Tree.java
@@ -1,17 +1,28 @@
 package task12;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 import java.util.Map.Entry;
 
 public class Tree {
     public static void main(String[] args) {
-        TreeMap<String,Integer> treeMap=new TreeMap<>();
-        treeMap.put("Benny",1);
-        treeMap.put("Annie",2);
-        treeMap.put("Daniel",3);
-        treeMap.put("Cathrine",4);
-        for (Entry<String,Integer> var : treeMap.entrySet()) {
+        TreeMap<Integer,String> treeMap=new TreeMap<>();
+        treeMap.put(1,"Benny");
+        treeMap.put(2,"Annie");
+        treeMap.put(3,"Cathrine");
+        treeMap.put(4,"Cathrine");
+        System.out.println("Printing based on sorted ID");
+        for (Entry<Integer,String> var : treeMap.entrySet()) {
+            System.out.println(var);
+        }
+
+        List<Map.Entry<Integer,String>> list=new ArrayList<>(treeMap.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        System.out.println("Printing based on sorted name");
+        for (Entry<Integer,String> var : list) {
             System.out.println(var);
         }
 
